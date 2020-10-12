be192cbe3e8fbce03f4736867d59951ea7736af7
Bernard Adanlessossi <adanlessossi@hotmail.com>
2020-10-12 16:26:16 +0200

    Updated the function

--- function.go
+++ function.go
@@ -5,15 +5,9 @@ import (
 	"math"
 )
 
-const delta = 1e-6
-
 func Sqrt(x float64) float64 {
-	z := x
-	n := 0.0
-	for math.Abs(n-z) > delta {
-		n, z = z, z-(z*z-x)/(2*z)
-	}
-	return z
+
+	return math.Sqrt(x)
 }
 
 func main() {
